aef50ebc804fdcf08b4c4c971bec01841c157a6e
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2022-11-28 17:32:49 +0000

    x

---
+++ test.ts
@@ -0,0 +1,12 @@
+import bin from "./glfw3_linux.js";
+
+const start = performance.now();
+const tmp = Deno.makeTempFileSync();
+Deno.writeFileSync(tmp, bin);
+const ffi = Deno.dlopen(tmp, {
+  glfwInit: { parameters: [], result: "i32" },
+} as const).symbols;
+Deno.removeSync(tmp);
+const end = performance.now();
+console.log(`Loaded in ${end - start}ms`);
+console.log(ffi.glfwInit());
